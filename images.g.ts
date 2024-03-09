20c95009490eaf35ca2d0e92a44c250486da4ac7
nandinikodati <162779202+nandinikodati@users.noreply.github.com>
2024-03-08 21:39:35 -0800

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+. . . . 9 9 9 9 9 9 9 9 9 . . . 
+. . 9 9 9 9 9 9 9 9 9 9 9 . . . 
+. . 9 9 9 9 9 9 9 9 9 9 9 9 . . 
+. . 9 9 9 f 9 9 9 f 9 9 9 9 . . 
+. 9 9 9 9 f 9 9 9 f 9 9 9 9 9 . 
+. 9 9 9 9 9 9 9 9 9 9 9 9 9 9 . 
+. 9 9 9 9 9 9 9 9 9 9 9 9 9 9 . 
+. 9 9 9 9 9 9 f 9 9 9 9 9 9 9 . 
+. 9 9 f 9 9 9 9 9 9 9 9 f 9 9 . 
+. 9 9 f f 9 9 9 9 9 9 f f 9 9 . 
+. 9 9 9 f f 9 9 9 9 f f 9 9 9 . 
+. 9 9 9 9 9 f f f f f 9 9 9 . . 
+. . 9 9 9 9 9 9 9 9 9 9 9 . . . 
+. . . 9 9 9 9 9 9 9 9 9 9 . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
